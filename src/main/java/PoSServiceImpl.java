d590775b2c2467cb8f28528dc90b9da33ce205d5
Reynir <reynir@reynir.net>
2016-12-03 11:23:33 +0100

    added impl of posservice

---
+++ src/main/java/PoSServiceImpl.java
@@ -0,0 +1,9 @@
+/**
+ * Created by reynir on 3.12.2016.
+ */
+public class PoSServiceImpl implements PoSService {
+    @Override
+    public String purchaseProduct(String productNumber) {
+        return "1234";
+    }
+}
